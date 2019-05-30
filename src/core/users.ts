09a19bd30a11640f2c9adc1eb46a0739171bdc88
Vecheslav Druzhbin <vecheslavdruzhbin@gmail.com>
2019-05-30 04:05:53 +0300

    roles & minor fixes

--- src/core/users.ts
+++ src/core/users.ts
@@ -3,7 +3,9 @@ import { prisma, User } from '../__generated__/prisma-client'
 export const getGameAddresses = async (): Promise<string[]> => {
   const games: User[] = await prisma.users({
     // TODO: will be added filter for game role
-    where: {},
+    where: {
+      role: 'GAME',
+    },
   })
 
   return games.map(game => game.address)
